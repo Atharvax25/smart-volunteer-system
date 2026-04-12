017ea3506ac44e2871474a9a2ebe62ac14ccce12
anish08-gh <anishghodke08@gmail.com>
2026-04-12 17:21:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/hackathon")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Backend + DB running");
+});
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
